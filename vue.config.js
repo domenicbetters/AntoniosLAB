212c18e6650fd53e4e464589884c52df4189e902
Domenic Betters <domenicbetters@yahoo.com>
2023-03-02 15:06:45 -0500

    fix?

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/AntoniosLAB/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/AntoniosLAB/dist" : "/",
 };
